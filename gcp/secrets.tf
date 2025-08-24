a1507bde241247e7de97b613b7f8d99a7a74eed3
Rahul-ramakrishnan06 <rahulkarthi06@gmail.com>
2025-08-24 15:13:57 +0530

    updating terraform plan and apply

--- gcp/secrets.tf
+++ gcp/secrets.tf
@@ -38,3 +38,4 @@ resource "google_secret_manager_secret_version" "db_password_version" {
   secret      = google_secret_manager_secret.db_password.id
   secret_data = random_password.db_password.result
 }
+
